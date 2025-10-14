8fed49d89c5ccac7af0bb1194498fec7090ac77c
Gastón Romero <romerogaston.programador@gmail.com>
2025-10-13 23:13:21 -0300

    fix: arreglo error de push

--- Taller1_progAvanzada/js/inicio_script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    const header = document.getElementById("encabezado");
-    if (window.scrollY > 50) { 
-        header.classList.add("scroll");
-    } else {
-        header.classList.remove("scroll");
-    }
-});
